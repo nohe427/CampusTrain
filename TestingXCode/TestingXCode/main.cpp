e45c089a61b043aa77df20482a3b9d913497ef24
Alexander Nohe <alex.nohe427@gmail.com>
2015-08-03 08:33:33 -0700

    First Commit
    :octocat:

---
+++ TestingXCode/TestingXCode/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TestingXCode
+//
+//  Created by Alexander Nohe on 8/3/15.
+//  Copyright (c) 2015 Alexander Nohe. All rights reserved.
+//
+
+#include <iostream>
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
